1eaecdd190116060512134e087cd15f22c09d01a
NikitosMetla <126060075+NikitosMetla@users.noreply.github.com>
2024-03-13 15:38:43 +0300

    Update users_stat.py

--- db/users_stat.py
+++ db/users_stat.py
@@ -61,11 +61,12 @@ class Users_stat:
             await self.edit_user_LLIC()
         return int(self.users.get(self.user_id).get("last_LLIC"))
 
-    async def edit_user_day(self):
+    async def edit_user_day(self, edit_day_stat: bool | None):
         next_day = int(self.users.get(self.user_id).get('day_now')) + 1
         self.users[self.user_id]["day_now"] = next_day
         self.users[self.user_id]["end_day"] = 0
-        await DAY_STAT().edit_day_stat(next_day)
+        if edit_day_stat is False
+            await DAY_STAT().edit_day_stat(next_day)
         await self.save_data()
 
     async def edit_user_end_day(self):
